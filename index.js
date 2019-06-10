dd2d400f61fe5fb876af3ced30fe40064ecb83b3
tiago-marques <tiago@zp.email>
2019-06-10 16:30:36 -0300

    1.0.2

--- index.js
+++ index.js
@@ -7,23 +7,32 @@ run().catch(error => console.error(error.stack));
 
 async function run() {
   const package = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'package.json'), 'UTF-8'))
-  nrc.run(`expo export --public-url ${package.homepage}`);
-  const homepage = package.homepage.replace('http', 'exp')
-  if(homepage.indexOf(homepage.length -1) === '/'){
-    homepage.slice(0, -1);
-  }
-  const androidIndexJsonURI = `${homepage}/android-index.json`
-  const iosIndexJsonURI = `${homepage}/ios-index.json`
 
-  const androidQrCode = await qrcode.toDataURL(androidIndexJsonURI)
-  const iosQrCode = await qrcode.toDataURL(iosIndexJsonURI)
+  var commands = [
+    `expo export --public-url ${package.homepage}`,
+    'gh-pages -d dist'
+  ];
+  var options = { cwd: path.resolve(__dirname) };
+  nrc.run(commands, options).then(function done(codes){
+    const homepage = package.homepage.replace('http', 'exp')
+    if(homepage.indexOf(homepage.length -1) === '/'){
+      homepage.slice(0, -1);
+    }
+    const androidIndexJsonURI = `${homepage}/android-index.json`
+    const iosIndexJsonURI = `${homepage}/ios-index.json`
   
-  fs.writeFileSync('./dist/index.html', `
-  <h1>Expo GH Pages</h1>
-  <h2>Android</h2>
-  <img src="${androidQrCode}">
-  <h2>iOS</h2>
-  <img src="${iosQrCode}">
-  `);
-  console.log('Wrote to ./index.html');
+    const androidQrCode = await qrcode.toDataURL(androidIndexJsonURI)
+    const iosQrCode = await qrcode.toDataURL(iosIndexJsonURI)
+    
+    fs.writeFileSync('./dist/index.html', `
+    <h1>Expo GH Pages</h1>
+    <h2>Android</h2>
+    <img src="${androidQrCode}">
+    <h2>iOS</h2>
+    <img src="${iosQrCode}">
+    `);
+  }, function err(err){
+    console.log(err)
+  })
+  
 }
