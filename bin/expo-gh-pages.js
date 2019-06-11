2fa1d78c519c8da6e15fa2146b89d84af9360664
tiago-marques <tiago@zp.email>
2019-06-11 12:36:47 -0300

    1.0.9

--- bin/expo-gh-pages.js
+++ bin/expo-gh-pages.js
@@ -19,9 +19,9 @@ function publish() {
 
     var options = { cwd: process.cwd() };
     nrc.run(commands, options).then(async function done(codes){
-      homepage = homepage.replace('http', 'exp')
-      const androidIndexJsonURI = `${homepage}/android-index.json`
-      const iosIndexJsonURI = `${homepage}/ios-index.json`
+      let exphomepage = homepage.replace('http', 'exp')
+      const androidIndexJsonURI = `${exphomepage}/android-index.json`
+      const iosIndexJsonURI = `${exphomepage}/ios-index.json`
     
       const androidQrCode = await qrcode.toDataURL(androidIndexJsonURI)
       const iosQrCode = await qrcode.toDataURL(iosIndexJsonURI)
@@ -32,17 +32,48 @@ function publish() {
 <html>
 
 <head>
-      <meta charset="UTF-8">
-      <title>expo-gh-pages</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css"
+        integrity="sha256-vK3UTo/8wHbaUn+dTQD0X6dzidqc5l7gczvH+Bnowwk=" crossorigin="anonymous" />
+    <meta charset="UTF-8">
+    <title>expo-gh-pages</title>
 </head>
 
 <body>
-      <h1>Expo GH Pages</h1>
-      <h2>Android</h2>
-      <img src="${androidQrCode}">
-      <h2>iOS</h2>
-      <img src="${iosQrCode}">
+    <div class="bd-example">
+        <section class="hero is-medium is-dark is-bold">
+            <div class="hero-body">
+                <div class="container">
+                    <p class="title">
+                        ${homepage}
+                    </p>
+                    <p class="subtitle">
+                        expo-gh-pages
+                    </p>
+                </div>
+            </div>
+        </section>
+    </div>
+    <div class="columns">
+        <div class="column">
+            <h1 class="title has-text-centered">
+                Android
+            </h1>
+            <img src="${androidQrCode}" style="display: block;
+            margin-left: auto;
+            margin-right: auto;" />
+        </div>
+        <div class="column">
+            <h1 class="title has-text-centered">
+                iOS
+            </h1>
+            <img src="${iosQrCode}" style="display: block;
+            margin-left: auto;
+            margin-right: auto;" />
+        </div>
+
+    </div>
 </body>
+
 </html>
       `);
       resolve()
